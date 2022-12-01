b859219bf8d34d8f259223bc3a82017e2802b19e
Vũ Minh Trí <58288020+vuminhtri@users.noreply.github.com>
2022-12-01 22:32:24 +0700

    Update hog.py

--- hog.py
+++ hog.py
@@ -6,3 +6,4 @@ img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
 img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
 img2 = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
 cv2.imwrite("hog_result.png", img2)
+
